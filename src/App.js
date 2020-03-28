a17826fe70a25f5536d6453e9751fd06d67feb3a
Dimochris <chrisdimo87@gmail.com>
2020-03-28 19:19:59 +0200

    antd library included

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "antd/dist/antd.css";
+import { Button } from 'antd';
+
+function App() {
+  return (
+    <div >
+      <h1>antd version: </h1>
+      <Button>dsds</Button>
+    </div>
+  );
+}
+
+export default App;
